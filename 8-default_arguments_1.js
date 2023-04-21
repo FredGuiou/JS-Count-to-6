10909facd208286dedadb9b7c485b27017567a56
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-04-21 15:28:56 +0200

    Done ✅ 8 of 10 default

---
+++ 8-default_arguments_1.js
@@ -0,0 +1,6 @@
+module.exports = function midpoint(low=0, high=1) {
+    return (low+high) / 2 ;
+};
+
+//Solution plus simple avec fonction anonyme
+// module.exports = (low = 0, high = 1) => (low + high) / 2;
